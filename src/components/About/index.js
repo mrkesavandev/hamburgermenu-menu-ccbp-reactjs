3b233175ae0342997ff222af67508c6ce5c61b96
kesavan <kesavangovindthan@gamil.com>
2023-09-15 13:02:36 +0000

    v1

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+import Header from '../Header'
+
+// Write your code here
+const About = () => (
+  <>
+    <Header />
+    <div className="about-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+        alt="about"
+        className="about-img"
+      />
+    </div>
+  </>
+)
+
+export default About
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+      alt="not found"
+      className="notfound-img"
+    />
+    <h1>Lost Your way? </h1>
+    <p>
+      Sorry, we cannot find that page. You will find lots to explore on the home
+      page
+    </p>
+  </div>
+)
+
+export default NotFound
